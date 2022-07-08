a58f751e1157d7c45c5541afbe9bd77e3ff4ef61
Abdullah Adeel <easyautosol@gmail.com>
2022-07-08 22:07:17 +0500

    ✅ dashboard page desing complete

--- apps/intercooler/src/layouts/headers/AppHeader.tsx
+++ apps/intercooler/src/layouts/headers/AppHeader.tsx
@@ -16,14 +16,21 @@ import Image from "next/image";
 import React from "react";
 import { AppHeaderNavLink } from "src/components/shared/Typography/NavLink";
 import ThemeToggler from "src/components/ThemeToggler/ThemeToggler";
+import Link from "next/link";
 
 interface AppHeaderProps {}
 
 export const AppHeader: React.FC<AppHeaderProps> = ({}) => {
   return (
     <Flex as="nav" px={10} py={4} gap={10} alignItems="center">
-      <Box>
-        <Image src={FantordLogo} alt="Logo" height="50px" width="50px" />
+      <Box
+        _hover={{
+          cursor: "pointer",
+        }}
+      >
+        <Link href="/dashboard">
+          <Image src={FantordLogo} alt="Logo" height="50px" width="50px" />
+        </Link>
       </Box>
       <Flex flex={1} gap={20}>
         <AppHeaderNavLink>Home</AppHeaderNavLink>
--- apps/intercooler/src/modules/dashboard/DashboardPage.tsx
+++ apps/intercooler/src/modules/dashboard/DashboardPage.tsx
@@ -1,4 +1,14 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Link from "next/link";
 import { PageComponent } from "src/types/PageComponent";
 interface DashboradPageProps {}
 
@@ -11,7 +21,43 @@ export const DashboardPage: PageComponent<DashboradPageProps> = ({}) => {
         lg: "60%",
       }}
     >
-      <h1>Dashboard</h1>
+      <HStack my={5} pr={7} justifyContent="space-between">
+        <Text fontSize="2xl">Fantord Profile</Text>
+        <Link href="/dashboard/profile">
+          <Button>Edit</Button>
+        </Link>
+      </HStack>
+      <VStack my={5} alignItems="flex-start">
+        <Text fontSize="2xl">My Servers</Text>
+        <Box bg="brand.800" w="100%" p={5} borderRadius={5}>
+          <Flex
+            justifyContent="space-between"
+            bg="brand.700"
+            p={2}
+            borderRadius={5}
+            my={2}
+          >
+            <HStack spacing={5}>
+              <Avatar size="md" src="https://bit.ly/sage-avatar" />
+              <Text>Fantord</Text>
+            </HStack>
+            <Button>Invite</Button>
+          </Flex>
+          <Flex
+            justifyContent="space-between"
+            bg="brand.700"
+            p={2}
+            borderRadius={5}
+            my={2}
+          >
+            <HStack spacing={5}>
+              <Avatar size="md" src="https://bit.ly/sage-avatar" />
+              <Text>Fantord</Text>
+            </HStack>
+            <Button>Manage</Button>
+          </Flex>
+        </Box>
+      </VStack>
     </Container>
   );
 };
